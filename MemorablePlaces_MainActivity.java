f09f6a24ad3d7baaba13dd40e3b8202777937459
SyedMdAshraf <55460309+SyedMdAshraf@users.noreply.github.com>
2020-02-12 14:32:53 +0530

    Create MemorablePlaces_MainActivity.java

---
+++ MemorablePlaces_MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.memorableplaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    static ArrayList<String> places=new ArrayList <String>();
+    static ArrayList<LatLng> locations=new ArrayList <LatLng>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listView);
+
+        places.add("Add a new place...");
+        locations.add(new LatLng(0,0));
+
+       arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, Integer.toString(position), Toast.LENGTH_SHORT).show();
+                Intent intentMap=new Intent(getApplicationContext(),MapsActivity.class);
+                intentMap.putExtra("placeNumber",position);
+
+                startActivity(intentMap);
+            }
+        });
+    }
+}
